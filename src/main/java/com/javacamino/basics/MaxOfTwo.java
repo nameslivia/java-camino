9d3f34429fe898d9922668ca90aa37aef70f756b
99 <cl9605697@gmail.com>
2026-04-27 09:54:17 +0800

    feat: (01-basucs) add method to return the larger of two numbers

---
+++ src/main/java/com/javacamino/basics/MaxOfTwo.java
@@ -0,0 +1,22 @@
+package com.javacamino.basics;
+
+public class MaxOfTwo {
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+//    三元運算子（ternary operator）： a > b 成立的話回傳 a，否則回傳 b
+//    public static int max(int a, int b) {
+//        return a > b ? a : b;
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(max(1, 9));
+        System.out.println(max(3, 7));
+        System.out.println(max(6, 6));
+    }
+}
